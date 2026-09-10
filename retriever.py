96b7029cb6d3000c718c724a65990e89ad0107fd
milan-ml <arotemilan@gmail.com>
2026-09-10 09:30:54 +0530

    Refactor Retriever class for improved readability

--- retriever.py
+++ retriever.py
@@ -1,47 +1,19 @@
-from app.ingestion.embeddings import (
-    create_query_embedding
-)
-
+from app.ingestion.embeddings import (create_query_embedding)
 
 class Retriever:
 
-    def __init__(
-        self,
-        vector_store,
-        chunks
-    ):
-
+    def __init__(self, vector_store, chunks):
         self.vector_store = vector_store
         self.chunks = chunks
 
-    def retrieve(
-        self,
-        query: str,
-        top_k: int = 3
-    ):
-
-        query_embedding = create_query_embedding(
-            query
-        )
-
-        distances, indices = (
-            self.vector_store.search(
-                query_embedding,
-                top_k
-            )
-        )
-
+    def retrieve(self, query: str, top_k: int = 3):
+        query_embedding = create_query_embedding(query)
+        distances, indices = (self.vector_store.search(query_embedding, top_k))
         results = []
 
-        for index, distance in zip(
-            indices[0],
-            distances[0]
-        ):
-
+        for index, distance in zip(indices[0], distances[0]):
             if index != -1:
-
                 chunk = self.chunks[index]
-
                 results.append(
                     {
                         "text": chunk["text"],
